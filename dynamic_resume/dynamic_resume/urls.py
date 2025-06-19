394ca8040acd66ce0b6caa8332430d6b8d4a087c
MehediAlam49 <mehedialam806@gmail.com>
2025-06-20 00:31:36 +0600

    complete mastering

--- dynamic_resume/dynamic_resume/urls.py
+++ dynamic_resume/dynamic_resume/urls.py
@@ -8,8 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('addResume', addResume, name='addResume'),
-    path('viewResume', viewResume, name='viewResume'),
-    path('editResume', editResume, name='editResume'),
-    path('deleteResume', deleteResume, name='deleteResume'),
+    path('addResume/', addResume, name='addResume'),
+    path('viewResume/', viewResume, name='viewResume'),
+    path('editResume/', editResume, name='editResume'),
+    path('deleteResume/', deleteResume, name='deleteResume'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
